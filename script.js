edad619a890f313d30627b12b962f5886746c715
buddymajki <78787757+buddymajki@users.noreply.github.com>
2023-08-14 11:30:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const apiKey = '803aad1a1d87010321d7bd4a0d759b4d';
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=47.0502&lon=8.3093&appid=${apiKey}&units=metric`;
+const currentApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=47.0502&lon=8.3093&appid=${apiKey}&units=metric`;
+const forecastApiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=47.0502&lon=8.3093&appid=${apiKey}&units=metric`;
 
-function updateWindData() {
-  fetch(apiUrl)
+function updateWindData(url, currentElementId, directionElementId) {
+  fetch(url)
     .then(response => response.json())
     .then(data => {
       const windSpeedMps = data.wind.speed;
       const windSpeedKph = (windSpeedMps * 3.6).toFixed(2); // Convert m/s to km/h
       const windDirection = data.wind.deg;
 
-      const windSpeedElement = document.getElementById('wind-speed');
-      const windDirectionElement = document.getElementById('wind-direction');
+      const windSpeedElement = document.getElementById(currentElementId);
+      const windDirectionElement = document.getElementById(directionElementId);
 
-      windSpeedElement.innerText = `Wind Speed: ${windSpeedKph} km/h fasza`;
+      windSpeedElement.innerText = `Wind Speed: ${windSpeedKph} km/h`;
       windDirectionElement.innerText = `Wind Direction: ${windDirection}°`;
     })
     .catch(error => {
@@ -20,5 +21,5 @@ function updateWindData() {
     });
 }
 
-updateWindData(); // Initial call
-setInterval(updateWindData, 5 * 60 * 1000); // Refresh every 5 minutes
+updateWindData(currentApiUrl, 'current-wind-speed', 'current-wind-direction'); // Current data
+updateWindData(forecastApiUrl, 'tomorrow-wind-speed', 'tomorrow-wind-direction'); // Tomorrow's forecast
